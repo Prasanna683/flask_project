49e561f25f12c5fe0e3e636811a1b52840fdb446
Prasanna <23a91a05e5@aec.edu.in>
2025-05-27 10:17:12 +0000

    Added bootstrap styling and jinjacodes

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs = [
+  {
+    'id': 1,
+    'title': 'Kakinada',
+    'price': 'Starting from Rs.75.00'
+  },
+  {
+    'id': 2,
+    'title': 'Vizag',
+    'price': 'Starting from Rs.100.00'
+  },
+  {
+    'id': 3,
+    'title': 'Rajahmundry',
+    'price': 'Starting from Rs.75.00'
+  }
+]
+
 @app.route("/")
 def home():
-  return render_template('home.html')
+  return render_template('home.html', branches=jobs)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
